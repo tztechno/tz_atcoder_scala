65a403a8052646d37d05693fb200f2687d633940
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-13 13:44:25 +0900

    Update vanishing.scala

--- vanishing.scala
+++ vanishing.scala
@@ -4,7 +4,10 @@ object Main extends App {
   val filePath = if (System.getProperty("os.name").toLowerCase.contains("linux")) "/dev/stdin" else "./input.txt"
   val input = Source.fromFile(filePath).getLines().next()
 
-  val Array(V, T, S, D) = input.split(" ").map(_.toInt)
+  val V = input.split(" ")(0).toInt
+  val T = input.split(" ")(1).toInt
+  val S = input.split(" ")(2).toInt
+  val D = input.split(" ")(3).toInt
 
   if (V * T <= D && D <= V * S) {
     println("No")
