3a79331a59d4a3a062dd3c57924baaa5119502f6
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-20 19:35:06 +0900

    Create follow.scala

---
+++ follow.scala
@@ -0,0 +1,19 @@
+
+//ABC182_A
+//Follow
+//input= 200 300
+
+object Answer {val from = new java.util.Scanner(System.in)
+def is(x:Any) = Console.println(x)}; import Answer.from._
+object Main extends App {
+    val a,b = nextInt
+    Answer is 2*a+100-b
+}
+
+###################################
+
+object Main extends App {
+  val sc = new java.util.Scanner(System.in)
+  val a, b = sc.nextInt
+  print(2 * a + 100 - b)
+}
