599a42971a51e385089529212eacb533938826f0
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-01-23 19:22:40 +0900

    Create conditioner.scala

---
+++ conditioner.scala
@@ -0,0 +1,28 @@
+//abc174_a conditioner.scala
+
+####################################
+object Main extends App {
+  val sc = new java.util.Scanner(System.in)
+  println {
+    if (sc.nextInt >= 30) "Yes" else "No"
+  }
+}
+####################################
+object Main extends App {
+
+    val A = scala.io.StdIn.readLine().trim
+    val X = A.toInt
+    if (X>=30)
+        println("Yes")
+    else
+        println("No")
+}
+####################################
+object Main extends App {
+    val A = scala.io.StdIn.readLine().trim
+    val X = A.toInt
+    val judge = (X >= 30).toInt
+    val ANS = Array("No", "Yes")
+    println(ANS(judge))
+}
+####################################
